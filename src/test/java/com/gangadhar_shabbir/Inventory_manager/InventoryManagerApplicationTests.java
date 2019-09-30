8cd2e8deb1ab8892f8af3e219e6302480840b5a0
Shabbir Hussain <shabbirh@qti.qualcomm.com>
2019-09-30 17:39:19 +0530

    Initial commit

---
+++ src/test/java/com/gangadhar_shabbir/Inventory_manager/InventoryManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gangadhar_shabbir.Inventory_manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InventoryManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
